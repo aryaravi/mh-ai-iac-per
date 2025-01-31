8babdeaf49f7caff210e1f26fd4ad7da3dbf3e79
aryaravi <aryaprasadr@gmail.com>
2025-01-31 14:13:34 -0500

    Update example2.tf

--- architecture-to-cloudformation/data/examples/example2.tf
+++ architecture-to-cloudformation/data/examples/example2.tf
@@ -1,122 +1,161 @@
-resource "aws_dynamodb_table" "example" {
-  name           = "example-table"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "id"
+provider "aws" {
+  region = "us-west-2"  # Set to your desired region
+}
+
+# Variables
+variable "dynamodb_table_name" {
+  description = "Name of the DynamoDB table"
+  type        = string
+  default     = "DataProcessingTable"
+}
+
+# DynamoDB Table with encryption at rest
+resource "aws_dynamodb_table" "dynamodb_table" {
+  name           = var.dynamodb_table_name
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 5
+  write_capacity = 5
 
   attribute {
     name = "id"
     type = "S"
   }
 
-  stream_enabled   = true
-  stream_view_type = "NEW_IMAGE"
+  hash_key = "id"
+
+  stream {
+    stream_view_type = "NEW_AND_OLD_IMAGES"
+  }
+
+  server_side_encryption {
+    enabled = true  # Encryption at rest enabled
+    sse_algorithm = "AES256"  # Specifying AES256 encryption algorithm
+  }
 
   tags = {
-    Name        = "example-table"
-    Environment = "dev"
+    Compliance = "FedRAMP"
   }
 }
 
+# DynamoDB Stream (Event Source for Lambda)
+resource "aws_lambda_event_source_mapping" "dynamodb_stream" {
+  event_source_arn = aws_dynamodb_table.dynamodb_table.stream_arn
+  function_name    = aws_lambda_function.processing_lambda_function.arn
+  starting_position = "LATEST"
+  batch_size       = 1
+}
+
+# Lambda Function for Processing DynamoDB Stream
+resource "aws_lambda_function" "processing_lambda_function" {
+  function_name = "LogFunction"
+  runtime       = "python3.12"
+  handler       = "index.lambda_handler"
+  role          = aws_iam_role.lambda_role.arn
+
+  environment {
+    variables = {
+      DYNAMODB_TABLE = var.dynamodb_table_name
+    }
+  }
+
+  code {
+    zip_file = <<ZIP_FILE
+import json
+import boto3
+
+def lambda_handler(event, context):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('DataProcessingTable')
+
+    for record in event['Records']:
+        if record['eventName'] == 'INSERT':
+            new_item = record['dynamodb']['NewImage']
+            item_id = new_item['id']['S']
+            # Process the new item
+            print(f"New item inserted: {item_id}")
+        elif record['eventName'] == 'MODIFY':
+            new_item = record['dynamodb']['NewImage']
+            old_item = record['dynamodb']['OldImage']
+            item_id = new_item['id']['S']
+            # Process the modified item
+            print(f"Item modified: {item_id}")
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Data processing completed successfully')
+    }
+ZIP_FILE
+  }
+
+  tags = {
+    Compliance = "FedRAMP"
+  }
+}
+
+# IAM Role for Lambda Function with least privilege access
 resource "aws_iam_role" "lambda_role" {
-  name = "lambda_role"
+  name = "lambda_execution_role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Action = "sts:AssumeRole"
         Effect = "Allow"
+        Action = "sts:AssumeRole"
         Principal = {
           Service = "lambda.amazonaws.com"
         }
-      },
+      }
     ]
   })
-
-  tags = {
-    Name        = "lambda_role"
-    Environment = "dev"
-  }
 }
 
-resource "aws_iam_policy" "lambda_policy" {
-  name        = "lambda_policy"
-  description = "IAM policy for Lambda to access DynamoDB and CloudWatch Logs"
+# Attach IAM policy to Lambda Role to allow access to DynamoDB stream
+resource "aws_iam_role_policy" "lambda_policy" {
+  name = "lambda_dynamodb_policy"
+  role = aws_iam_role.lambda_role.id
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
+        Effect = "Allow"
         Action = [
-          "dynamodb:GetItem",
-          "dynamodb:PutItem",
-          "dynamodb:UpdateItem",
-          "dynamodb:DeleteItem",
-          "dynamodb:Scan",
-          "dynamodb:Query",
-          "dynamodb:DescribeTable",
-          "dynamodb:ListStreams",
+          "dynamodb:DescribeStream",
           "dynamodb:GetRecords",
           "dynamodb:GetShardIterator",
-          "dynamodb:DescribeStream",
           "dynamodb:ListStreams"
         ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-      {
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ]
-        Effect   = "Allow"
-        Resource = "*"
+        Resource = aws_dynamodb_table.dynamodb_table.stream_arn
       }
     ]
   })
+}
 
+# CloudWatch Log Group for Lambda with FedRAMP compliant retention
+resource "aws_cloudwatch_log_group" "lambda_log_group" {
+  name              = "/aws/lambda/${aws_lambda_function.processing_lambda_function.function_name}"
+  retention_in_days = 90  # FedRAMP requires 90 days log retention
   tags = {
-    Name        = "lambda_policy"
-    Environment = "dev"
+    Compliance = "FedRAMP"
   }
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
-  role       = aws_iam_role.lambda_role.name
-  policy_arn = aws_iam_policy.lambda_policy.arn
+# Lambda Permission for DynamoDB to trigger Lambda
+resource "aws_lambda_permission" "lambda_permission" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.processing_lambda_function.function_name
+  principal     = "dynamodb.amazonaws.com"
+  source_arn    = aws_dynamodb_table.dynamodb_table.stream_arn
 }
 
-resource "aws_lambda_function" "example" {
-  function_name = "example_lambda"
-  role          = aws_iam_role.lambda_role.arn
-  handler       = "index.handler"
-  runtime       = "python3.8"
-  filename      = "lambda_function_payload.zip"
-
-  environment {
-    variables = {
-      TABLE_NAME = aws_dynamodb_table.example.name
-    }
-  }
-
-  tags = {
-    Name        = "example_lambda"
-    Environment = "dev"
-  }
+# Outputs
+output "dynamodb_table_name" {
+  description = "The name of the DynamoDB table"
+  value       = aws_dynamodb_table.dynamodb_table.name
 }
 
-resource "aws_lambda_event_source_mapping" "dynamodb_trigger" {
-  event_source_arn = aws_dynamodb_table.example.stream_arn
-  function_name    = aws_lambda_function.example.arn
-  starting_position = "LATEST"
-}
-
-resource "aws_cloudwatch_log_group" "example" {
-  name = "/aws/lambda/example_lambda"
-
-  tags = {
-    Name        = "example_log_group"
-    Environment = "dev"
-  }
+output "lambda_function_arn" {
+  description = "The ARN of the Lambda function"
+  value       = aws_lambda_function.processing_lambda_function.arn
 }
